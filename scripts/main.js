83ceddcfcedf709fca8864ffe8f2e1ba46665457
Steven Huang <steven.charles.huang@gmail.com>
2019-09-23 17:25:19 -0400

    added boilerplate

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+const searchBtn   = document.querySelector('.search-btn');
+const searchInput = document.querySelector('#search-input');
+const gallery     = document.querySelector('.gallery');
+
+
+
+
+
