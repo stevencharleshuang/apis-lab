33a230773fd17ebf5f0836f39a507a76f8c3d1ed
Steven Huang <steven.charles.huang@gmail.com>
2019-09-23 17:48:58 -0400

    light styling, fetch successful GET, successful renders of results

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,36 @@ const searchBtn   = document.querySelector('.search-btn');
 const searchInput = document.querySelector('#search-input');
 const gallery     = document.querySelector('.gallery');
 
+let resultsArr = [];
 
+const handleSubmit = (e) => {
+  e.preventDefault();  
+  
+  let query = searchInput.value;
+
+  console.log('search submitted!');
+  fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=RRdCt59QG1Ut01EyxPVOO4Rgp1WiGJhR`)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response)
+      resultsArr = response.data;
+      // console.log(results);
+
+      populateGallery(resultsArr);
+    })
+    .catch(err => console.log(err));
+}
+
+const populateGallery = (results) => {
+  results.forEach(img => {
+    let gif = document.createElement('img');
+    gif.setAttribute('src', `https://media.giphy.com/media/${img.id}/giphy.gif`);
+    gallery.append(gif);
+  });
+}
+
+
+searchBtn.addEventListener('click', handleSubmit);
 
 
 
