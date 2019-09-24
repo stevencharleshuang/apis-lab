0e92ddc06d30b2050962fb0bae66532055f51de6
Steven Huang <steven.charles.huang@gmail.com>
2019-09-24 10:27:08 -0400

    implemented logic for user to load more gifs, updated styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,38 @@
 const searchBtn   = document.querySelector('.search-btn');
 const searchInput = document.querySelector('#search-input');
 const gallery     = document.querySelector('.gallery');
+const main        = document.querySelector('main');
 
 let resultsArr = [];
+let pagination = [];
+let query = '';
+let offset = 0;
 
 const handleSubmit = (e) => {
   e.preventDefault();  
   
-  let query = searchInput.value;
+  query = searchInput.value;
 
   fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=RRdCt59QG1Ut01EyxPVOO4Rgp1WiGJhR`)
     .then(response => response.json())
     .then(response => {
       console.log(response)
       resultsArr = response.data;
+      pagination = response.pagination;
       populateGallery(response.data);
+    })
+    .catch(err => console.log(err));
+}
+
+const handleLoadMore = (e) => {
+  e.preventDefault();
+  offset += 25;
+
+  fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&offset=${offset}&api_key=RRdCt59QG1Ut01EyxPVOO4Rgp1WiGJhR`)
+    .then(response => response.json())
+    .then(response => {
+      console.log('Load More Response', response);
+      addMoreToGallery(response.data);
     })
     .catch(err => console.log(err));
 }
@@ -26,8 +44,26 @@ const populateGallery = (results) => {
     gif.setAttribute('src', `https://media.giphy.com/media/${img.id}/giphy.gif`);
     gallery.append(gif);
   });
+
+  console.log(pagination);
+
+  if (pagination.total_count > 25) {
+    const loadMoreBtn = document.createElement('button');
+    loadMoreBtn.innerText = 'Load More...';
+    loadMoreBtn.setAttribute('class', 'load-more-btn btn');
+    main.append(loadMoreBtn);
+    
+    loadMoreBtn.addEventListener('click', handleLoadMore);
+  }
 }
 
+const addMoreToGallery = (results) => {
+  results.forEach(img => {
+    let gif = document.createElement('img');
+    gif.setAttribute('src', `https://media.giphy.com/media/${img.id}/giphy.gif`);
+    gallery.append(gif);
+  })
+}
 
 searchBtn.addEventListener('click', handleSubmit);
 
