67ba0d83c19671351fc2ecb733b0fb6ec592f6ff
Steven Huang <steven.charles.huang@gmail.com>
2019-09-24 08:53:48 -0400

    added styles, refactored search input location to header

--- scripts/main.js
+++ scripts/main.js
@@ -9,20 +9,18 @@ const handleSubmit = (e) => {
   
   let query = searchInput.value;
 
-  console.log('search submitted!');
   fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=RRdCt59QG1Ut01EyxPVOO4Rgp1WiGJhR`)
     .then(response => response.json())
     .then(response => {
       console.log(response)
       resultsArr = response.data;
-      // console.log(results);
-
-      populateGallery(resultsArr);
+      populateGallery(response.data);
     })
     .catch(err => console.log(err));
 }
 
 const populateGallery = (results) => {
+  gallery.innerHTML = '';
   results.forEach(img => {
     let gif = document.createElement('img');
     gif.setAttribute('src', `https://media.giphy.com/media/${img.id}/giphy.gif`);
